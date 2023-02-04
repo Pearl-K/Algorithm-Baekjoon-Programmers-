ee59fd0f49cce589886adde089095c2dc31c91ac
Jenett <80253713+Pearl-K@users.noreply.github.com>
2023-02-04 20:41:04 +0900

    [Silver III] Title: 주유소, Time: 112 ms, Memory: 46260 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,12 +2,10 @@ import sys
 N = int(sys.stdin.readline())
 road = list(map(int, sys.stdin.readline().split()))
 price = list(map(int, sys.stdin.readline().split()))
-result = 0
+res = 0
+m = price[0]
 for i in range(N-1):
-    if price[i] < price[i+1]:
-        result += price[i]*(road[i]+road[i+1])
-        if (i + 1) == len(road) - 1:
-            break
-    else:
-        result += price[i]*road[i]
-print(result)
+    if price[i] < m:
+        m = price[i]
+    res += m*road[i]
+print(res)
