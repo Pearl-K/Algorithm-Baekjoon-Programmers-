e05ffc65547a06e3c4f6891c330742ccf33bd936
Pearl <80253713+Pearl-K@users.noreply.github.com>
2023-05-31 19:37:06 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 388 ms, Memory: 131236 KB -BaekjoonHub

---
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+N, E = map(int, input().split())
+graph = [[]*(N+1) for i in range(N+1)]
+
+for i in range(E): #그래프 인접리스트 만들기
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+    graph[v].append((u, w))
+V1, V2 = map(int, input().split())
+
+import heapq as hq
+INF = sys.maxsize
+
+def dijkstra(graph, start):
+    dist = [INF] *(N+1)
+    dist[start] = 0 # 시작지점 0으로 시작
+    PQ = [] # 우선순위 큐 만들기
+    hq.heappush(PQ, (0, start)) #시작 노드와 시작 dist값 넣기
+    #거리가 먼저 들어가야 한다.
+
+    while PQ:
+        distance, node = hq.heappop(PQ)
+
+        if dist[node] < distance: #기존 거리가 새로운 거리보다 더 작을 때
+            continue
+
+        for i in graph[node]:
+            alt = i[1] + distance
+            if alt < dist[i[0]]:
+                dist[i[0]] = alt
+                hq.heappush(PQ, (alt, i[0]))
+    return dist
+
+res = dijkstra(graph, 1)
+one = dijkstra(graph, V1)
+two = dijkstra(graph, V2)
+
+V1Path = res[V1] + one[V2] + two[N]
+V2Path = res[V2] + one[N] + two[V1]
+result = min(V1Path, V2Path)
+print(result if result < INF else -1)
