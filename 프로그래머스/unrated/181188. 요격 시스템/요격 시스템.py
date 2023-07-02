6740e9b46289cfa7b81844ecd70e8c6117a28bf0
Pearl <80253713+Pearl-K@users.noreply.github.com>
2023-07-02 23:11:10 +0900

    [unrated] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,11 +1,13 @@
 def solution(targets):
     answer = 0
-    targets.sort(key = lambda x: [x[1], x[0]])
+    # 1. 끝 순서대로 sorting
+    targets.sort(key = lambda x:(x[1], x[0]))
     
+    # 2. 초기화
     s = e = 0
-    for target in targets:
-        if target[0] >= e:
+    for t in targets:
+        if t[0] >= e:
             answer += 1
-            e = target[1]
+            e = t[1] #범위 업로드
 
-    return answer 
+    return answer
