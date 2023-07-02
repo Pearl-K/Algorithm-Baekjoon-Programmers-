77c9da5b60621faacd46ec63c336336ba2e76b3b
Pearl <80253713+Pearl-K@users.noreply.github.com>
2023-07-02 23:33:03 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 705.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -1,10 +1,7 @@
 import math
-
 def solution(r1, r2):
-    #1. 내부 점 찾기
     in_cnt = 0
     
-    #2. bound 하한-상한 정해서 찾기
     for x in range(1, r2 + 1):
         y_max = math.floor(math.sqrt(r2**2 - x**2))
         y_min = 0 if x >= r1 else math.ceil(math.sqrt(abs(r1**2 - x**2)))
