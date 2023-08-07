b146e3d3c909cec709ab2a2c6940abe6572fa533
Pearl <80253713+Pearl-K@users.noreply.github.com>
2023-08-07 17:09:16 +0900

    [Gold V] Title: 택배 배송, Time: 288 ms, Memory: 51560 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+#시작 시간 PM 1:34, 제출 시간 PM 1:55
+
+N, M = map(int, input().split())
+graph = [[]*(N+1) for i in range(N+1)]
+
+for _ in range(M):
+    u, v, w = map(int, input().split())
+    # PQ 우선순위를 위해 가중치를 먼저 넣기
+    graph[u].append((w, v))
+    graph[v].append((w, u))
+
+import heapq as hq
+INF = sys.maxsize
+
+def dijkstra(start):
+    dist = [INF]*(N+1)
+    dist[start] = 0
+    PQ = []
+    hq.heappush(PQ, (0, start))
+
+    while PQ:
+        now_d, node = hq.heappop(PQ)
+
+        if dist[node] < now_d:
+            continue
+
+        for n in graph[node]:
+            alt = n[0] + now_d
+            if alt < dist[n[1]]:
+                dist[n[1]] = alt
+                hq.heappush(PQ, (alt, n[1]))
+    return dist
+
+result = dijkstra(1)
+print(result[N])
