8a85d6792f7c56a1af1a6acde532269408a6b2f9
Pearl <80253713+Pearl-K@users.noreply.github.com>
2023-08-30 18:04:34 +0900

    [Gold V] Title: Coins, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/3067. Coins/Coins.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int T, N, K;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> T;
+    for(int t=0; t < T; t++){
+        cin >> N;
+        vector<int> c(N+1); //coin
+
+        for(int i=1; i <=N; i++){
+            cin >> c[i];
+        }
+
+        cin >> K;
+        vector<int> dp(K+1);
+        dp[0] = 1; //초기화
+
+        for(int i=1; i <= N; i++){
+            for(int j=c[i]; j <= K; j++){
+                dp[j] += dp[j-c[i]];
+            }
+        }
+        cout << dp[K] << "\n";
+    }
+    return 0;
+}
