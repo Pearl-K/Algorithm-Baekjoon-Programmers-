5c49e876c04b75c0e7dd664fa6d06838b7dfbca5
Pearl <80253713+Pearl-K@users.noreply.github.com>
2023-09-02 18:32:54 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+arr = list(map(str, input().split()))
+
+arr.sort(key=lambda x:x*11, reverse=True)
+res = ''
+for i in arr:
+    res += i
+    
+if res == '0'*len(res):
+    print(0)
+else:
+    print(res)
