2670ff5720037a369e59189b33f0f1d96522e197
Pearl <80253713+Pearl-K@users.noreply.github.com>
2023-10-25 22:16:54 +0900

    [Silver III] Title: queuestack, Time: 236 ms, Memory: 47084 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+M = int(input())
+C = list(map(int, input().split()))
+
+from collections import deque
+q = deque()
+
+for i in range(N):
+  if A[i] == 0:
+    q.appendleft(B[i])
+  else:
+      if q == []:
+        print(*C)
+        sys.exit()
+
+for i in range(M):
+  q.append(C[i])
+  print(q.popleft(), end = " ")
