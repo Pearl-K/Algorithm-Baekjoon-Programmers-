e9c7be2d8af7460ce64f549c4ab59b952c0fabeb
Pearl <80253713+Pearl-K@users.noreply.github.com>
2023-12-01 22:54:24 +0900

    [Silver I] Title: Torus Travel, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/19737. Torus Travel/Torus Travel.py
+++ 백준/Silver/19737. Torus Travel/Torus Travel.py
@@ -1,24 +1,19 @@
-import sys
 from math import pi
-input = sys.stdin.readline
 r, R, n = map(int, input().split())
 res = 0
 
-# 도시 간 이동
+# city 간 이동, 안쪽 라인(R-r) 이동, N/S 라인 이동(R)
 city = (1/4)*2*pi*r
-
-# in-line, out-line
 in_ln = (1/n)*2*pi*(R-r)
 out_ln = (1/n)*2*pi*R
 
 if n == 1:
     res = city
-else: # out-line, in-line 번갈아 타는 경우, i1 점에서 시작함. 처음엔 무조건 out-line 탐
+else: # out-line & in-line 번갈아 타는 경우 or i점으로만 이동해야 하는 경우
     if (n-1) % 2 == 0:
         one = ((n-1)//2)*out_ln + ((n-1)//2)*in_ln + (city*n)
     else:
         one = ((n-1)//2+1)*out_ln + ((n-1)//2)*in_ln + (city*n)
     two = city*n + (city + in_ln)*(n-1)
     res = min(one, two)
-
 print(res)
