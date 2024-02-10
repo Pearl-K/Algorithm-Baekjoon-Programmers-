893fed70de96a60cc57b97b5e56aef9cf03aaa30
Pearl <80253713+Pearl-K@users.noreply.github.com>
2024-02-10 18:38:25 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 56 ms, Memory: 32140 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+st = input().rstrip()
+alp = [[0 for _ in range(26)] for _ in range(len(st))]
+
+for i in range(len(st)):
+    idx = ord(st[i])-97
+    if i == 0:
+        alp[i][idx] += 1
+    else:
+        alp[i][idx] += 1
+        for j in range(26):
+            alp[i][j] += alp[i-1][j]
+
+N = int(input())
+for i in range(N):
+    a, l, r = input().split()
+    res = 0
+    idx = ord(a)-97
+
+    if int(l) == 0:
+        res = alp[int(r)][idx]
+    else:
+        res = alp[int(r)][idx]-alp[int(l)-1][idx]
+    print(res)
