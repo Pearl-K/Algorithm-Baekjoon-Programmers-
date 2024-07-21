1a5a6657c4cc65754ed92357c94f738ebae172a4
Pearl <80253713+Pearl-K@users.noreply.github.com>
2024-07-22 00:00:51 +0900

    [Gold V] Title: 홀수 홀릭 호석, Time: 0 ms, Memory: 2028 KB, Score: 10 point -BaekjoonHub

--- 백준/Gold/20164. 홀수 홀릭 호석/홀수 홀릭 호석.cc
+++ 백준/Gold/20164. 홀수 홀릭 호석/홀수 홀릭 호석.cc
@@ -3,7 +3,8 @@
 using namespace std;
 
 string S;
-int retMin = 1e9;
+
+int retMin = 9999999;
 int retMax = 0;
 
 int oddCnt(string str){
@@ -22,25 +23,27 @@ pair<int, int> dfs(string str, int cnt){
     cnt += oddCnt(str);
 
     if(sLen == 1){
-        retMin = retMax = cnt;
+        retMin = min(retMin, cnt);
+        retMax = max(retMax, cnt);
         return {retMin, retMax};
+
     } else if (sLen == 2){
         int tmp = 0;
-        for(char s : str) tmp += s-'0';
+        for(char s : str) tmp += (s-'0');
         return dfs(to_string(tmp), cnt);
     } else {
-        for(int i = 0; i < sLen-2; i++){
+        for(int i = 1; i < sLen-1; i++){
             for(int j = i+1; j < sLen-1; j++){
 
-                string s1 = str.substr(0,i+1); // 0 ~ i
-                string s2 = str.substr(i+1,j-i); //i+1 ~ j
-                string s3 = str.substr(j+1); // j ~ str.length
+                string s1 = str.substr(0,i); // 0 ~ i
+                string s2 = str.substr(i,j-i); //i+1 ~ j
+                string s3 = str.substr(j, sLen-j); // j ~ str.length
                 string sum = to_string(stoi(s1) + stoi(s2) + stoi(s3));
 
-                pair<int,int> val = dfs(sum,cnt);
+                pair<int,int> tmp = dfs(sum, cnt);
 
-                retMin = min(retMin,val.first);
-                retMax = max(retMax,val.second);
+                retMin = min(retMin, tmp.first);
+                retMax = max(retMax, tmp.second);
             }
         }
 
