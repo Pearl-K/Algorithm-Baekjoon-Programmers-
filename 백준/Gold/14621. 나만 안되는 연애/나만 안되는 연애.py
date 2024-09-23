09380e2e2c0ee2688466e3701ed456bf6b10efea
Pearl <80253713+Pearl-K@users.noreply.github.com>
2024-09-23 23:30:47 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 56 ms, Memory: 36236 KB -BaekjoonHub

---
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+
+N, M = map(int, input().split())
+gender = ['_']
+gender += list(input().split())
+
+tmp = []
+for _ in range(M):
+    tmp.append((map(int, input().split())))
+
+edges = []
+for a, b, dist in tmp:
+    if gender[a] != gender[b]:
+        edges.append((dist, a, b))
+
+edges.sort()
+parents = [i for i in range(N+1)]
+
+def find_p(n):
+    if parents[n] == n:
+        return n
+    parents[n] = find_p(parents[n])
+    return parents[n]
+
+def union(a,b):
+    a = find_p(a)
+    b = find_p(b)
+    if a<b:
+        parents[b] = a
+    elif a>b:
+        parents[a] = b
+
+res = 0
+for dist, a, b in edges:
+    if find_p(a) != find_p(b):
+        union(a,b)
+        res += dist
+
+root = find_p(1)
+for i in range(1, N+1):
+    if root != find_p(i):
+        res = -1
+        break
+print(res)
