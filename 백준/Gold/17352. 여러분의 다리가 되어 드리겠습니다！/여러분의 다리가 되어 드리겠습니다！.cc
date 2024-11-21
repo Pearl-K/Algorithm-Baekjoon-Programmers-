9ffd7f09dec72a11ebe24c7467435a30391e0c01
Pearl <80253713+Pearl-K@users.noreply.github.com>
2024-11-22 00:21:09 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 68 ms, Memory: 4084 KB -BaekjoonHub

---
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+const int MAX = 300001;
+int N;
+int parent[MAX];
+
+int find(int x) {
+	if (parent[x] == x)
+		return x;
+	return parent[x] = find(parent[x]);
+}
+
+void union_p(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x == y)
+		return;
+	parent[y] = x;
+}
+
+bool check(int x, int y) {
+	x = find(x);
+	y = find(y);
+    
+	if (x == y) return true;
+    else return false;
+}
+
+int main() {
+	ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+    cin >> N;
+    
+	for (int i = 0; i < MAX; i++) parent[i] = i;
+    
+	for (int i = 0; i < N-2; i++) {
+		int u, v; 
+        cin >> u >> v;
+		union_p(u, v);
+	}
+    
+	for (int i = 2; i <= N; i++) {
+		if (!check(1, i)) {
+			cout << 1 << " " << i;
+			break;
+		}
+	}
+    return 0;
+}
