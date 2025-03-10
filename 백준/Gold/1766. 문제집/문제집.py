81cfa0e32540d297f105563dc464f4b51fc24eed
Pearl <80253713+Pearl-K@users.noreply.github.com>
2025-03-10 23:33:37 +0900

    [Gold II] Title: 문제집, Time: 184 ms, Memory: 40880 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.py
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -1,33 +1,30 @@
 import sys
+import heapq as hq
 input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
-
+sys.setrecursionlimit(10**6+1)
 N, M = map(int, input().split())
 graph = [[] for _ in range(N+1)]
-indgr = [0]*(N+1)
+ind = [0]*(N+1)
 
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
-    indgr[b] += 1
-#print(graph)
-#print(indgr)
+    ind[b] += 1
 
 for i in range(1, N+1):
     graph[i].sort()
 
-import heapq as hq
-H = []
+heap = []
 for i in range(1, N+1):
-    if indgr[i] == 0:
-        hq.heappush(H, i)
+    if ind[i] == 0:
+        hq.heappush(heap, i)
 
 for i in range(N):
-    now = hq.heappop(H)
-    print(now, end =' ')
-    graph[now].sort()
-
-    for nxt in graph[now]:
-        indgr[nxt] -= 1
-        if indgr[nxt] == 0:
-            hq.heappush(H, nxt)
+    node = hq.heappop(heap)
+    print(node, end=" ")
+    graph[node].sort()
+    
+    for nxt in graph[node]:
+        ind[nxt] -= 1
+        if ind[nxt] == 0:
+            hq.heappush(heap, nxt)
