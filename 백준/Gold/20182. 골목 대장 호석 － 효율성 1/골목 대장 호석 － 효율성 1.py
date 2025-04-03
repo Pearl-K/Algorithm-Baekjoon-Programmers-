a2b5ac7a4636d41d0c6b0d46c933c1c6626af40b
Pearl <80253713+Pearl-K@users.noreply.github.com>
2025-04-04 00:31:07 +0900

    [Gold III] Title: 골목 대장 호석 - 효율성 1, Time: 2828 ms, Memory: 163596 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.py
+++ 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.py
@@ -1,56 +1,24 @@
 import sys
 input = sys.stdin.readline
-
-INF = sys.maxsize
-N, M, A, B, C = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-cost = []
-
-for _ in range(M):
-    u, v, w = map(int, input().split())
-    graph[u].append([w, v])
-    graph[v].append([w, u])
-    cost.append(w)
-
+from collections import defaultdict
 import heapq as hq
+N, M, A, B, C = map(int, input().split())
+G, D = defaultdict(list),[2000000]*(N+1)
+for _ in range(M):
+    u,v,w=map(int, input().split())
+    G[u].append((v,w))
+    G[v].append((u,w))
+    
+h = [(0,0,A)]
 
-def dijkstra(limit):
-    dist = [INF for _ in range(N+1)]
-    dist[A] = 0 #시작점 초기화
-
-    PQ = []
-    hq.heappush(PQ, [0, A])
-
-    while PQ:
-        nowcost, nownode = hq.heappop(PQ)
-
-        if dist[nownode] < nowcost:
-            continue
-
-        for nxtcost, nxtnode in graph[nownode]:
-            if dist[nxtnode] > nxtcost + nowcost and nxtcost <= limit:
-                # 간선 비용 제한 이내일 때 이동
-                dist[nxtnode] = nxtcost + nowcost
-                hq.heappush(PQ, [nxtcost + nowcost, nxtnode])
-
-    if dist[B] > C:
-        return INF
-    else:
-        return dist[B]
-
-# 간선 비용 최소/최대 값을 기록한 것으로 이분 탐색 하기
-cost.sort()
-st, ed = 0, len(cost)-1
-res = INF
-
-while st <= ed:
-    mid = (st + ed) // 2
-    total = dijkstra(cost[mid])
-
-    if total == INF:
-        st = mid + 1
-    else:
-        ed = mid - 1
-        res = min(res, cost[mid])
-
-print(res if res < INF else -1)
+while h:
+    m, s, u = hq.heappop(h)
+    if u==B:
+        print(m)
+        sys.exit()
+    for v,w in G[u]:
+        t = max(m,w)
+        if D[v]>t and s+w<=C:
+            D[v]=t
+            hq.heappush(h, (t, s+w, v))
+print(-1)
