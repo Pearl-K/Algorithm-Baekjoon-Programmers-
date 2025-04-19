ea335080cd08b0c8feacc99d6445639e4ab330e0
Pearl <80253713+Pearl-K@users.noreply.github.com>
2025-04-19 23:44:58 +0900

    [Gold III] Title: 과제, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.py
+++ 백준/Gold/13904. 과제/과제.py
@@ -1,28 +1,21 @@
 import sys
 input = sys.stdin.readline
-
 N = int(input())
 arr = []
-visit = [False] * 1001
+vst = [False]*1001
 res = 0
-
 for i in range(N):
     d, w = map(int, input().split())
     arr.append((d, w))
-
 arr.sort(key=lambda x: x[1], reverse=True)
 
 for day, worth in arr:
     idx = day
-
-    #가능한 날짜 체크
-    while idx > 0 and visit[idx]:
+    while idx > 0 and vst[idx]:
         idx -= 1
-
     if idx == 0:
         continue
     else:
-        visit[idx] = True
+        vst[idx] = True
         res += worth
-
 print(res)
